33026717d86caad1038a9c968f95f2f7690cacb4
Vladimir <bigcorvet@gmail.com>
2023-03-06 17:28:26 +0200

    added func update user

--- server/routes/users.js
+++ server/routes/users.js
@@ -15,3 +15,26 @@ router.get('/', async(req, res) => {
     }
 })
 
+//обновить данные пользователя
+router.put('/:id', async (req, res) => {
+    if(req.body.id == req.params.id || req.body.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10)
+                req.body.password = await bcrypt.hash(req.body.password, salt)
+            } catch (error) {
+                return res.status(500).json(error)
+            }
+        }
+        try {
+            const user = User.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            })
+            res.status(200).json('Аккаунт успешно обновлен')
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+    } else {
+        return res.status(403).json('Вы можете обновить только свой аккаунт!')
+    }
+})
