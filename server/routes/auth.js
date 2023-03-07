12221a478eaed0bad2924a7700ea1fde49ee2059
Vladimir <bigcorvet@gmail.com>
2023-03-07 10:25:59 +0200

    Added user login function

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -20,3 +20,22 @@ router.post("/register", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+//Логин
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("Пользователь не найден!");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(404).json("Неверные учетные данные");
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
