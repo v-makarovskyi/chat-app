943cfa9a0e70bd0d08c7ed7d3bc55e826f5e4fe3
Vladimir <bigcorvet@gmail.com>
2023-03-07 10:42:18 +0200

    Added post update function

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -12,3 +12,18 @@ router.post('/', async (req, res) => {
         res.status(500).json(error)
     }
 })
+
+//обновить пост
+router.put('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (post.userId == req.body.userId) {
+            await Post.updateOne({ $set: req.body })
+            res.status(200).json('Пост успешно обновлен')
+        } else {
+            res.status(403).json('Вы можете обновить только свои посты')
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
